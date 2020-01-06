e20e4b3f9b66f2edb04dfe357b667d1e0b1d9771
Carlos Eduardo <cadu_medeirossantos2011@hotmail.com>
2020-01-05 23:45:44 -0300

    Primeia classe criada

---
+++ src/xadrez/Programa.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xadrez;
+
+import boardgame.Position;
+
+/**
+ *
+ * @author Cadu
+ */
+public class Programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Position p = new Position(3, 4);
+        
+        System.out.println(p.toString());
+    }
+    
+}
