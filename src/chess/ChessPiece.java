9ecb7a848898eebf92ebd89bec91390205c59c0d
Carlos Eduardo <cadu_medeirossantos2011@hotmail.com>
2020-01-12 18:27:25 -0300

    Contagem de movimento de peça

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,6 +16,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
     
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Color color, Board board) {
         super(board);
@@ -30,9 +31,21 @@ public abstract class ChessPiece extends Piece{
         return ChessPosition.fromPosition(this.position);
     }
     
+    public int getMoveCount(){
+        return this.moveCount;
+    }
+    
     protected boolean isThereOpponentPiece(Position position){
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         
         return p != null && p.getColor() != this.color;
+    }
+    
+    public void increaseMoveCount(){
+        this.moveCount++;
+    }
+    
+    public void decreaseMoveCount(){
+        this.moveCount--;
     }
 }
