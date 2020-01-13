4a8f3b29f1e353edd3b59cf81ec53ab6a4ea095b
Carlos Eduardo <cadu_medeirossantos2011@hotmail.com>
2020-01-12 22:41:10 -0300

    Jogada especial Rogue

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -7,6 +7,7 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,12 @@ import chess.Color;
  * @author Cadu
  */
 public class King extends ChessPiece{
+    
+    private ChessMatch chessMatch;
 
-    public King(Board board, Color color) {
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(color, board);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -29,6 +33,11 @@ public class King extends ChessPiece{
         ChessPiece p = (ChessPiece) this.getBoard().piece(position);
         
         return p == null || p.getColor() != this.getColor();
+    }
+    
+    private boolean testRookCasting(Position position){
+        ChessPiece p = (ChessPiece) this.getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == this.getColor() && p.getMoveCount() == 0;
     }
     
     @Override
@@ -83,6 +92,32 @@ public class King extends ChessPiece{
         p.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
         if(this.getBoard().positionExists(p) && this.canMove(p)){
             mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        // Movimento especial Roque
+        if(this.getMoveCount() == 0 && !this.chessMatch.getCheck()){
+            Position posT1 = new Position(this.position.getRow(), this.position.getColumn() + 3);
+            
+            if(this.testRookCasting(posT1)){
+                Position p1 = new Position(this.position.getRow(), this.position.getColumn() + 1);
+                Position p2 = new Position(this.position.getRow(), this.position.getColumn() + 2);
+                
+                if(this.getBoard().piece(p1) == null && this.getBoard().piece(p2) == null){
+                    mat[this.position.getRow()][this.position.getColumn() + 2] = true;
+                }
+            }
+            
+            Position posT2 = new Position(this.position.getRow(), this.position.getColumn() - 4);
+            
+            if(this.testRookCasting(posT1)){
+                Position p1 = new Position(this.position.getRow(), this.position.getColumn() - 1);
+                Position p2 = new Position(this.position.getRow(), this.position.getColumn() - 2);
+                Position p3 = new Position(this.position.getRow(), this.position.getColumn() - 3);
+                
+                if(this.getBoard().piece(p1) == null && this.getBoard().piece(p2) == null && this.getBoard().piece(p3) == null){
+                    mat[this.position.getRow()][this.position.getColumn() - 2] = true;
+                }
+            }
         }
         
         return mat;
